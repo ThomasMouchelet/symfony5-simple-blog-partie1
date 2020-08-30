96088914b31a0a63e6de676fdce6f9447db785e5
ThomasMouchelet <mouchelet.thomas@gmail.com>
2020-08-30 08:09:31 +0200

    Symfony Blog Partie 1

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->setTitle($faker->sentence($nbWords = 2, $variableNbWords = true))
+                ->setContent($faker->sentence($nbWords = 10, $variableNbWords = true))
+                ->setAuthor($faker->name())
+                ->setCreatedAt($faker->dateTimeBetween('-6 months'));
+
+            $manager->persist($post);
+        }
+
+        $manager->flush();
+    }
+}
